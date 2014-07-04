294de721ed38e6471b3ba4fa71b8d9846f31b5bd
Alex Arwine <arwineap@gmail.com>
2014-07-03 18:53:07 -0700

    last.fm api sucks

--- init.py
+++ init.py
@@ -1,8 +1,62 @@
 #!/usr/bin/env python
+
 import lastfm
+import taglib
+import os
+
+
+def flacProfileDir(dir):
+    dirprof = {'TITLE': [], 'TRACKNUMBER': []}
+    for dirpath, dirname, files in os.walk(dir):
+        for filen in files:
+            if filen.endswith('.flac'):
+                filepath = os.path.join(dirpath, filen)
+                flacfile = taglib.File(filepath)
+                tags = flacfile.tags
+                for key in tags:
+                    if key not in dirprof:
+                        dirprof[key] = {}
+                    if key == 'TITLE':
+                        dirprof[key].append(tags[key][0])
+                    elif key == 'TRACKNUMBER':
+                        tracknumber = int(tags[key][0])
+                        dirprof[key].append(tracknumber)
+                    else:
+                        if tags[key][0] not in dirprof[key]:
+                            dirprof[key][tags[key][0]] = 0
+                        dirprof[key][tags[key][0]] += 1
+    return dirprof
+
+
+def analyze(profile):
+    result = {'tracks': len(profile['TRACKNUMBER']), 'artist': {}, 'date': {}, 'album': {}, 'genre': {}}
+    for artist in profile['ARTIST']:
+        result['artist'][artist] = float(profile['ARTIST'][artist])/result['tracks']
+    for date in profile['DATE']:
+        result['date'][date] = float(profile['DATE'][date])/result['tracks']
+    for album in profile['ALBUM']:
+        result['album'][album] = float(profile['ALBUM'][album])/result['tracks']
+    for genre  in profile['GENRE']:
+        result['genre'][genre] = float(profile['GENRE'][genre])/result['tracks']
+    return result
+
+sampledirprofile = flacProfileDir('/home/alex/git/agent-runkle/sampleflac/samplealbum/')
+
+print('sample dir profile:', sampledirprofile)
+analyzedprof = analyze(sampledirprofile)
+print(analyzedprof)
+
+guessedalbum = max(analyzedprof['album'], key=lambda k: analyzedprof['album'][k])
+guessedartist = max(analyzedprof['artist'], key=lambda k: analyzedprof['artist'][k])
 
 x = lastfm.api('./creds.json')
 pm = {}
-pm['method'] = 'auth.getToken'
-result = x.call(pm)
-print(result)
+pm['method'] = 'album.search'
+pm['album'] = guessedalbum
+
+searchresults = x.doCall(pm)
+print(searchresults['results']['albummatches']['album'][0])
+print(searchresults['results']['albummatches']['album'][1])
+
+
+#f = taglib.File("")
--- lastfm.py
+++ lastfm.py
@@ -19,31 +19,24 @@ class api:
         return creds
 
     def auth_getToken(self):
+        # get lastfm token
+        # Must use call, and set things up manually
+        # doCall may call getToken causing massive recursion
         pm = {}
         pm['method'] = 'auth.getToken'
+        pm['api_key'] = self.api_key
+        pm['format'] = 'json'
         self.token = self.call(pm)
         self.tokentime = time.time()
 
     def tokenExpire(self):
+        # Tokens expire after an hour.
         if time.time()-self.tokentime < 3600:
             return False
         return True
 
     def call(self, parameters):
-        # Step two of auth process
-        # auth.getToken
-        # takes one parameter:
-        # api_key
-        # method = 'auth.getToken'
-        # parameters = {'method' : 'auth.getToken', 'api_key' : self.api_key}
-        #
-        # xml, you've been good to us, but it's time to die.
-        if 'format' not in parameters:
-            parameters['format'] = 'json'
-        if 'api_key' not in parameters:
-            parameters['api_key'] = self.api_key
-        if self.token is None or self.tokenExpire():
-            self.auth_getToken()
+        # raw call method
         domain = 'http://ws.audioscrobbler.com'
         apiversion = '2.0'
         querystring = urllib.parse.urlencode(parameters)
@@ -52,3 +45,18 @@ class api:
         str_response = response.readall().decode('utf-8')
         obj = json.loads(str_response)
         return obj
+
+    def doCall(self, parameters):
+        # helper call method
+        # if you don't have a token, get one
+        if self.tokentime is None:
+            self.auth_getToken()
+        # if token is stale, replace it
+        if self.tokenExpire():
+            self.auth_getToken()
+        # xml, you've been good to us, but it's time to die.
+        if 'format' not in parameters:
+            parameters['format'] = 'json'
+        if 'api_key' not in parameters:
+            parameters['api_key'] = self.api_key
+        return self.call(parameters)
